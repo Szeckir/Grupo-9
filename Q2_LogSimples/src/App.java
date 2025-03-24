b66b29821c2fd88af3b56e4629a3b98273c405ee
Marina S Bon <141246618+marinasbon@users.noreply.github.com>
2025-03-24 20:53:36 +0000

    exercicio 2

--- Q2_LogSimples/src/App.java
+++ Q2_LogSimples/src/App.java
@@ -1,9 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        LogSimples log = new LogSimples();
+        LogSimples log = getInstance();
         log.log("Mensagem de alerta 1");
         log.log("Mensagem de alarme 23");
         log.log("Mensagem de PANICO!!");
+
+        LogSimples log2 = getInstance();
 
         for(String m:log){
             System.out.println(m);
--- Q2_LogSimples/src/LogSimples.java
+++ Q2_LogSimples/src/LogSimples.java
@@ -4,9 +4,17 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class LogSimples implements Iterable<String>{
+    private static LogSimples instancia;
     private List<String> mensagens;
 
-    public LogSimples(){
+    public static LogSimples getInstance(){
+        if (instancia == null) {
+            instancia = new CodificadorFactory();
+        }
+        return instancia;
+    }
+
+    private LogSimples(){
         mensagens = new LinkedList<>();
     }
 
